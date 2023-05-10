f8d7cc2dc527b1b73792dbd0e74941084a3c7e17
Avery Berryman <averyberryman@gmail.com>
2023-05-10 18:41:38 -0400

    Add iteration 0 and 1 solutions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return "Delicious" + " " + menuItemName;
 }
+
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients (toppings, ingredients) {
+  if (!ingredients.includes(toppings)) {
+    ingredients.push(toppings)
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
+
+function decreasePrice(price) {
+  (price *= .9)
+  return price
+}
+
+function createRecipe(title, ingredients, type) {
+ return {
+  title: title,
+  ingredients: ingredients,
+  type: type
+}
+}
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
