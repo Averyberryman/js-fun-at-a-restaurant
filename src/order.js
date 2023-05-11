b10cadeaba0a1477cb733f5033c53e9a87ba9713
Avery Berryman <averyberryman@gmail.com>
2023-05-10 22:20:24 -0400

    Add takeOrder, refundOrder, listItems functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order)
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNumber === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice(i, 1)
+    }
+  }
+
+}
+
+function listItems(deliveryOrders) {
+  var items = ''
+  var lastIndex = deliveryOrders.length -1
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i] === deliveryOrders[lastIndex]) {
+      items += deliveryOrders[i].item
+    } else {
+      items += deliveryOrders[i].item
+      items += ', '
+    }
+  }
+  return items
+}
+
+function searchOrder () {
+
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
